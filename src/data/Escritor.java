0fb76e5e12f58913a175d076d49f62439ca0101a
Samuel David Lopez Sarabia <lopezs.samueld@gmail.com>
2024-04-21 16:43:41 -0500

    Se crearon los metodos de las interfaces

--- src/data/Escritor.java
+++ src/data/Escritor.java
@@ -1,9 +1,14 @@
 package data;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Samuel David López Sarabia
  */
 public interface Escritor {
+    
+    public abstract void escribir(ArrayList<String> rutaArchivo) throws IOException ;
     
 }
--- src/data/Lector.java
+++ src/data/Lector.java
@@ -1,9 +1,14 @@
 package data;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Samuel David López Sarabia
  */
 public interface Lector {
+    
+    public abstract ArrayList<String> leer(String rutaArchivo) throws IOException;
     
 }
